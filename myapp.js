f0996f9b97112705d5c5c02aff9686db084f52af
Vivian <viviankariuki028@gmail.com>
2023-03-07 15:21:34 +0300

    my first commit

---
+++ myapp.js
@@ -0,0 +1,34 @@
+let marks =100;
+
+let x = prompt("enter marks");
+alert(marks);
+//or is || and is &&
+if (x > 100 &&x < 10)
+{
+    
+    if(x > 70)
+    {
+        alert("greater than 70");
+        
+    }
+    else if(x < 70 && x >= 60)
+    {
+    alert("less than 70 and greater or equal to 60")
+    
+    }
+    else if(x < 60 && x >= 50)
+    {
+        alert("less than 60 and greater or equal to 50")
+    }
+    else if(x <50 && x >=40)
+    {
+        alert("less than 50 and greater or equal to 40")
+    }
+    else if(x < 40)
+    {
+        alert("less than 40")
+    }
+
+
+} 
+alert(Error)
